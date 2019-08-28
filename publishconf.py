44d5b9bf947b4a54f5dc0b6f26026234c62cde5b
Gerardo Ortega <geraldavid7@gmail.com>
2019-08-27 23:07:07 -0600

    Adjusting URL

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://notebook.g3ortega.com'
+SITEURL = 'http://notebook.g3ortega.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
