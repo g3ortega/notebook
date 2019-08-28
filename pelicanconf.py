166b59da5182926ff9c36aadab7ef1b120060a74
Gerardo Ortega <geraldavid7@gmail.com>
2019-08-27 22:47:13 -0600

    First test

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,9 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATH = './plugins'
+PLUGINS = ['ipynb.markup']
+
+THEME = 'themes/pelican-alchemy/alchemy'
